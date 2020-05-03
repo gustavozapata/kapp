341113c47c9c2a41b5c85cd25bac5365d31a3b67
Gustavo Zapata <tavordie@hotmail.com>
2020-05-03 01:43:56 +0100

    add layout and styling for main and all the api calls

--- src/index.js
+++ src/index.js
@@ -2,17 +2,16 @@ const axios = require("axios");
 const electron = require("electron");
 const remote = electron.remote;
 
-const start = document.querySelector("#start");
 const power = document.querySelector("#power");
 
-const menu = document.querySelectorAll("nav ul li");
 const items = document.querySelector(".items");
 
-start.addEventListener("click", () => {
-  axios.get("http://localhost:4000/api/v1/assets").then((res) => {
-    let data = res.data.data;
-    items.innerHTML = populate(data);
-  });
+const menu = document.querySelectorAll("nav ul li");
+
+axios.get("http://localhost:4000/api/v1/assets").then((res) => {
+  document.querySelector("#spinner").style.display = "none";
+  let data = res.data.data;
+  items.innerHTML = populate(data);
 });
 
 power.addEventListener("click", () => {
@@ -23,6 +22,8 @@ power.addEventListener("click", () => {
 menu.forEach((item) => {
   item.addEventListener("click", () => {
     if (item.className === "active") return false;
+
+    getSelectedItems(item.textContent);
     let image = item.querySelector("img").src;
     let no_active = document.querySelector(".active img").src;
 
@@ -41,10 +42,30 @@ menu.forEach((item) => {
   });
 });
 
+async function getSelectedItems(item) {
+  items.style.display = "none";
+  document.querySelector("#spinner").style.display = "block";
+
+  let name = item.toLowerCase().trim();
+  let num = name.split(" ");
+  name = num.length > 1 ? num[1] : name;
+
+  await axios.get(`http://localhost:4000/api/v1/${name}`).then((res) => {
+    items.style.display = "block";
+    document.querySelector("#spinner").style.display = "none";
+    items.innerHTML = populate(res.data.data);
+  });
+}
+
 function populate(data) {
   let element = "";
   data.forEach((el) => {
-    element += `<div><h3>${el.name}</h3><p>${el.description}</p><p>${el.currentlyAt.city}</p></div>`;
+    element += `<div class="item-container"><div class="item-icon"><p>${el.name.substring(
+      0,
+      2
+    )}</p></div><div class="item-info"><p class="item-name">${el.name}</p><p>${
+      el.currentlyAt.city
+    }</p><span>${el.keywords[0]}</span></div></div>`;
   });
   return element;
 }
